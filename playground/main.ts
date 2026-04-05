82b2223fc65bbd7722e9b5f1459070ef9eae384a
purupurupu <purupurupu0prog@gmail.com>
2026-04-05 23:09:15 +0900

    chore: simplify playground to use default options

--- playground/main.ts
+++ playground/main.ts
@@ -3,14 +3,5 @@ import { VueRenderDiagnostics } from '../src/index.ts';
 import App from './App.vue';
 
 const app = createApp(App);
-app.use(VueRenderDiagnostics, {
-  enabled: true,
-  updateLogInterval: 5,
-  onLog: (log) => {
-    console.table(log.metrics);
-    if (log.issues.length > 0) {
-      console.warn('Issues:', log.issues);
-    }
-  },
-});
+app.use(VueRenderDiagnostics, { enabled: true });
 app.mount('#app');
