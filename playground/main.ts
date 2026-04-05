d087bea361f999bc04395b92aece76ce57fa058f
purupurupu <purupurupu0prog@gmail.com>
2026-04-05 23:42:43 +0900

    chore: add updateLogInterval to playground to enable update logs

--- playground/main.ts
+++ playground/main.ts
@@ -3,5 +3,5 @@ import { VueRenderDiagnostics } from '../src/index.ts';
 import App from './App.vue';
 
 const app = createApp(App);
-app.use(VueRenderDiagnostics, { enabled: true });
+app.use(VueRenderDiagnostics, { enabled: true, updateLogInterval: 10 });
 app.mount('#app');
